e741a745aae90463adb1581a6ecac7a07517b48b
akki0824 <thoutamakhilsai1997@gmail.com>
2023-09-25 22:12:37 +0530

    changes-3

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -25,3 +25,11 @@ output "oidc_id" {
   value = aws_iam_openid_connect_provider.default.id
   
 }*/
+
+output "identity" {
+  value = aws_eks_cluster.sta_cluster.identity[0].oidc[0].issuer
+}
+
+output "oidc_id" {
+  value = aws_iam_openid_connect_provider.default.id
+}
