6fc036ef05b0a02f7d4adcfa46f448fb30376f1e
akki0824 <thoutamakhilsai1997@gmail.com>
2023-09-27 11:17:34 +0530

    output3

--- eks/main.tf
+++ eks/main.tf
@@ -12,7 +12,7 @@ resource "aws_eks_node_group" "worker-group" {
   cluster_name    = aws_eks_cluster.sta_cluster.name
   node_group_name = var.node_group_name
   node_role_arn   = aws_iam_role.worker-role.arn
-  subnet_ids      = [var.subnet_ids]
+  subnet_ids      = var.subnet_ids
   scaling_config {
     desired_size = var.desired_size
     max_size     = var.max_size
